3cdba23647ca4c4473daaa0b96e53c4fcdfc665b
Fakher Halim <fakher@tpgxml.com>
2021-01-23 11:50:58 -0600

    Added the build task (tasks.json) <CTRL><SHIFT>B

--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@
 #include "person.h"
 
 using namespace std;
-
+//First run <CTRL><SHIFT>B to build after any change. Then <F5> to run with debugging
 int main(int argc, char* args[]){
     Person* person = new Person("Fakher Halim");
-    cout << "Hello " << person->getName() << endl;
+    cout << "Hello Fakher, " << person->getName() << endl;
     delete person;
     return 0;
 }
